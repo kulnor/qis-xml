7bff8ff787771cd522975f91285e2e61c382f540
kulnor <pascal.heus@gmail.com>
2016-09-04 12:36:46 -0600

    Separated YANG grouping and data types definition from main model

--- yang/yangdocs.sh
+++ yang/yangdocs.sh
@@ -2,10 +2,19 @@
 # Generates .yang files derived formats, example, documentation
 # Requires pyang (https://github.com/mbj4668/pyang)
 
+includes="plgah-qis-types.yang"
 for f in *.yang; do
-    pyang -f tree -o "${f%.yang}.txt" $f 
-    pyang -f jstree -o "${f%.yang}.html" $f
-    pyang -f uml -o "${f%.yang}.uml" $f
-    pyang -f sample-xml-skeleton -o "${f%.yang}.sample.xml" $f
+    echo $f
+    if [ $f != "plgah-qis-types.yang" ] ; then
+        echo "-->txt"
+        pyang -f tree -o "${f%.yang}.txt" $f $includes
+        echo "-->jstree"
+        pyang -f jstree -o "${f%.yang}.html" $f $includes
+        #echo "-->uml"
+        #pyang -f uml -o "${f%.yang}.uml" $f
+        echo "-->sample-xml"
+        pyang -f sample-xml-skeleton -o "${f%.yang}.sample.xml" $f $includes
+    fi
+    echo "-->yin"
     pyang -f yin -o "${f%.yang}.yin.xml" $f
 done
